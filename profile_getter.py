8f91aa238cb87a41192c149fb7f883f544e896a0
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2018-04-30 15:52:21 -0500

    added login to fb

--- profile_getter.py
+++ profile_getter.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 """ Accesses a user's facebook profile
 	Note that selenium requires geckodriver 
@@ -8,19 +9,31 @@ from selenium.webdriver.common.keys import Keys
 	https://github.com/mozilla/geckodriver/releases
 """
 class FB_Profile_Driver():
+	""" Note that to access a user's friends list,
+		we need to enter a facebook account
+	"""
 	def __init__(self, username, password):
 		self.username = username
 		self.password = password
+		self.access_profile()
+
+	""" Enters a user's facebook profile"""
+	def access_profile(self):
 		self.browser = webdriver.Firefox()
-
-	def enter_Facebook(self):
 		self.browser.get('https://www.facebook.com/')
-		email = self.browser.find_element_by_id('email')
-		email.send_keys(self.username)
-		elem = self.browser.find_element_by_id('pass')
-		elem.send_keys(self.password)
-		elem.send_keys(Keys.RETURN)
-		self.browser.close()
+		sleep(1)
+		username_input = self.browser.find_element_by_id('email')
+		username_input.send_keys(self.username)
+		sleep(1)
+		password_input = self.browser.find_element_by_id('pass')
+		password_input.send_keys(self.password)
+		login_input = self.browser.find_element_by_id('loginbutton')
+		login_input.click()
+		sleep(1)
 
-f = FB_Profile_Driver('email', 'password')
-f.enter_Facebook()
+	""" Accesses friends, assumes access_profile already called"""
+	def access_friend(self):
+		profile_link = self.browser.find_element_by_id('findFriendsNav')
+
+
+f = FB_Profile_Driver('cs232facebook@gmail.com', 'Facebook1!')
