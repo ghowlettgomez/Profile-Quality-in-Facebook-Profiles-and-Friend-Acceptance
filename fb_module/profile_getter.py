ab5e49431a23a5edeb803f1c97199956061b8f6b
Eli Bogursky <Eli Bogursky>
2018-05-20 16:45:50 -0500

    resizing photo

--- fb_module/profile_getter.py
+++ fb_module/profile_getter.py
@@ -24,12 +24,15 @@ class FB_Profile_Driver():
 		self.username = username
 		self.password = password
 		self.editor = HTML_Editor()
+        self.resizer = Photo_Resizer()
 		self.access_profile()
 
 	"""Given the url of a participant in the study, return a friend of theirs"""
 	def run(self, profile_url, path):
 		friends_list = self.access_friends_of_profile(profile_url)
 		friend_body_html = self.access_friend(friends_list)
+        imgurl = self.editor.saveProfilePic(friend_body_html)
+        self.resizer.resizeimage(imgurl)
 		edited_body_html = self.editor.replaceProfilePic(friend_body_html)
 		self.load_body_html(edited_body_html)
 		self.take_screenshot(path)
