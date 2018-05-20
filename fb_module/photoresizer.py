6b84cd5dcbd6f39ecdcfc802b65995d3ffd5611b
Eli Bogursky <Eli Bogursky>
2018-05-20 17:37:34 -0500

    fix image save

--- fb_module/photoresizer.py
+++ fb_module/photoresizer.py
@@ -5,9 +5,9 @@ import urllib
 class Photo_Resizer(object):
 
     def resizeimage(imgurl, path):
-        urllib.request.urlretrieve(imgurl, path + "/imageForResizing.jpg")
+        urllib.request.urlretrieve(imgurl, path + "imageForResizing.jpg")
 
-        with open(path + '/imageForResizing.jpg', 'r+b') as f:
+        with open(path + 'imageForResizing.jpg', 'r+b') as f:
             with Image.open(f) as image:
                 cover = resizeimage.resize_cover(image, [100, 100])
-                cover.save(path + '/resizedImage.jpg', image.format)
+                cover.save(path + 'resizedImage.jpg', image.format)
