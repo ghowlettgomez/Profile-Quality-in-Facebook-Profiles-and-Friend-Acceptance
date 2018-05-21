90ad351851e8af0b94483dae42946c37fa4df2f0
Jake Zane <jzane16@kentdenver.org>
2018-05-21 10:15:36 -0500

    added handling for that one random indexerror

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from fb_module.profile_getter import FB_Profile_Driver
 
 f = FB_Profile_Driver('cs232facebook@gmail.com', 'Facebook1!')
-f.run('https://www.facebook.com/profile.php?id=100008426081012', '/tmp/screenshot.png')
+while True:
+    try:
+        f.run('https://www.facebook.com/profile.php?id=100008426081012', '/tmp/screenshot.png')
+        break
+    except IndexError:
+        f.run('https://www.facebook.com/profile.php?id=100008426081012', '/tmp/screenshot.png')
