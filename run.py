97a10cad7e46db5a8088d9516567eff0e26bb16b
Jake Zane <jzane16@kentdenver.org>
2018-05-21 15:31:28 -0500

    recursive handler

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from fb_module.profile_getter import FB_Profile_Driver
 
 f = FB_Profile_Driver('cs232facebook@gmail.com', 'Facebook1!')
-while True:
-    try:
-        f.run('https://www.facebook.com/profile.php?id=100008426081012', '/tmp/')
-        break
-    except IndexError:
-        f.run('https://www.facebook.com/profile.php?id=100008426081012', '/tmp/')
+
+x = 0
+
+def runner():
+    while True:
+        try:
+            f.run('https://www.facebook.com/profile.php?id=100008426081012', '/tmp/screenshot.png')
+            break
+        except IndexError:
+            runner()
+
+runner()
